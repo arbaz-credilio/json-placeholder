8fd452d09a51e4b77b6855f8da01db01046ba0f2
md arbaz  alam <md.alam.27.2@mountblue.tech>
2024-04-15 17:27:04 +0530

    update project

--- pages/user/[id].vue
+++ pages/user/[id].vue
@@ -8,19 +8,29 @@
       ><span>Phone No. : </span> {{ user?.phone }}</UContainer
     >
     <UContainer class="mb-8"
-      ><span>City: </span>{{ user?.address?.city }}</UContainer
-    >
+      ><span>City: </span>{{ user?.address?.city }}</UContainer>
     <UButton color="red" class="ml-8" @click="deleteAndNavigate(id)"
-      >Delete User</UButton
+      >Delete User</UButton>
+    <UButton color="red" class="ml-8" @click="isOpen = true">Edit User</UButton
     >
   </UContainer>
+  <div>
+
+    <UModal v-model="isOpen">
+      <UInput type="text" v-model="user.name" class="mb-4" />
+      <UInput type="text" v-model="user.email" class="mb-4" />
+      <UInput type="text" v-model="user.phone" class="mb-4" />
+      <UInput type="text" v-model="user.address.city" />
+    </UModal>
+  </div>
 </template>
 
 <script setup lang="ts">
 let users = getAllUsers();
+const isOpen = ref(false)
 const route = useRoute();
 const id: string | string[] = route.params.id;
-const user = users.value.find((each: any) => each.id.toString() === id);
+const user = users.value && users.value.find((each: any) => each.id.toString() === id);
 
 const deleteAndNavigate = (id: string | string[]) => {
   deleteUser(id);
